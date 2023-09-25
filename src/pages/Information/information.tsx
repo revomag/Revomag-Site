a1e7f1e243d2dd2cec1525ebb4a09184871fe103
Brandon Cantello <bcantello@gmail.com>
2023-09-24 20:17:05 -0700

    Updates note on information page

--- src/pages/Information/information.tsx
+++ src/pages/Information/information.tsx
@@ -5,7 +5,7 @@ export default function Information() {
     return (
         <>
             <NavBar/>
-            <div>This is the Information page</div>
+            <div>This is the Information page. This should actually not be a page, and should instead be a dropdown menu with a bunch of other pages.</div>
         </>
     );
 }
