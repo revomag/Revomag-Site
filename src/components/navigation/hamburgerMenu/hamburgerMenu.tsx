44bd6a6a62f3f0088c6031a33e42189255d66dc2
Brandon Cantello <bcantello@gmail.com>
2023-11-18 15:41:46 -0800

    Styling for Mouth Breathing

--- src/components/navigation/hamburgerMenu/hamburgerMenu.tsx
+++ src/components/navigation/hamburgerMenu/hamburgerMenu.tsx
@@ -33,51 +33,44 @@ export default function HamburgerMenu(): ReactElement {
                 </div>
                 <ul id={'toggle'} className={hamburgerToggle}>
                     <li>
-                        <Link className={"ham-nav-link"} to={"/home"} style={{textDecoration: "none"}}>
+                        <Link className={"ham-nav-link"} to={"/home"}>
                             HOME
                         </Link>
                     </li>
                     <li>
-                        <Link className={"ham-nav-link"} to={"/how-it-works"} style={{textDecoration: "none"}}>
+                        <Link className={"ham-nav-link"} to={"/how-it-works"}>
                             HOW IT WORKS
                         </Link>
                     </li>
                     <li>
-                        <Link className={"ham-nav-link"} to={"/contact"} style={{textDecoration: "none"}}>
-                            CONTACT
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={"ham-nav-link"} to={"/about"} style={{textDecoration: "none"}}>
-                            ABOUT NICOLE
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={"ham-nav-link"} to={"/refer-a-patient"} style={{textDecoration: "none"}}>
-                            REFER A PATIENT
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={'ham-nav-link'} to={'/resources'} style={{textDecoration: 'none'}}>
-                            RESOURCES
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={"ham-nav-link"} to={"/book-appointment"} style={{textDecoration: "none"}}>
+                        <Link className={"ham-nav-link"} to={"/book-appointment"}>
                             BOOK APPOINTMENT
                         </Link>
                     </li>
                     <li>
-                        <div className={"ham-nav-dropdown-content ham-nav-link"}>
-                            INFORMATION <img className={"ham-chevron"} src={RightChevron} alt={"right chevron"}/>
+                        <Link className={"ham-nav-link"} to={"/contact"}>
+                            CONTACT
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={"ham-nav-link"} to={"/about"}>
+                            ABOUT NICOLE
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={"ham-nav-link"} to={"/refer-a-patient"}>
+                            REFER A PATIENT
+                        </Link>
+                    </li>
+                    <li>
+                        <div className={'ham-add-info'}>
+                            Additional Information:
                         </div>
-                        <ul className={"ham-nav-dropdown"}>
-                            <li className={"nav-dropdown-content"}>
-                                <Link className={"ham-nav-link"}>
-                                    MOUTH BREATHING
-                                </Link>
-                            </li>
-                        </ul>
+                    </li>
+                    <li>
+                        <Link className={"ham-nav-link ham-add-info-link"} to={"/mouth-breathing"}>
+                            Mouth Breathing
+                        </Link>
                     </li>
                 </ul>
             </div>
