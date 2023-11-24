f2fda98f430f23791ff9ca300b56447af4447188
Brandon Cantello <bcantello@gmail.com>
2023-11-24 14:32:51 -0800

    images wip

--- src/pages/bookAppointment/bookAppointment.tsx
+++ src/pages/bookAppointment/bookAppointment.tsx
@@ -2,11 +2,14 @@ import {ReactElement} from "react";
 import Navigation from "../../components/navigation/navigation";
 import Footer from "../../components/footer/footer";
 import './bookAppointment.scss';
+import Banner from "../../components/banner/banner";
+import ScheduleAppointment from "../../assets/bannerImages/appointmentBanner.jpeg";
 
 export default function BookAppointment(): ReactElement {
     return (
         <>
             <Navigation/>
+            <Banner imageSrc={ScheduleAppointment} imageAlt={'calendar'} message={'Book Appointment'}/>
             <div>This is the Book Appointment page</div>
             <Footer/>
         </>
