2314a2249c49c73b145e5a2918556fca8b7646a1
Brandon Cantello <bcantello@gmail.com>
2023-12-04 13:52:21 -0800

    Tongue thrust desktop complete

--- src/pages/Information/tongueThrust/tongueThrust.tsx
+++ src/pages/Information/tongueThrust/tongueThrust.tsx
@@ -24,13 +24,23 @@ export default function TongueThrust(): ReactElement {
             <section className={'tongue-thrust-content-container'}>
                 <h2>Tongue Thrust and Orthodontics</h2>
                 <div className={'tongue-thrust-content-wrapper'}>
-                    <div>
+                    <div className={'tongue-thrust-content-copy'}>
                         <p>A tongue thrust describes a swallowing pattern in which an individual pushes the tongue against or between the teeth during swallowing.</p>
                         <p>Our teeth will move with consistent light pressure and our tongue is actually quite strong. Specifically, it takes 1.5 grams of consistent force to move a tooth. The tongue has a median average force of 499 grams and can go up to 700-800 grams of force. Individuals with a tongue thrust swallowing pattern will commonly have anterior open bite. This is where the front teeth do not touch when biting down with the back teeth.</p>
                         <p>This can affect the ability to have proper oral rest posture, correct tongue posture, ability to breathe through your nose and swallowing.</p>
                         <p>Simply put, form follows function. Or put another way, deformities follow dysfunction. If the muscles of your tongue are not functioning properly, it will affect multiple facial and airway structures. If tongue thrust is not corrected, this can lead to underdevelopment of craniofacial structures, high vaulted palate, sleep disordered breathing and dental problems such as crowding, periodontal disease and decay.</p>
                         <p>Orofacial myofunctional therapy is a method that can be used for eliminating a tongue thrust. It is an exercise based treatment that teaches patients how to use their tongue and facial muscles properly.</p>
                         <p>An Orofacial Myofunctional Therapist (OMT) can be an instrumental figure in helping a patient learn how to re-train the muscles of the face and tongue. OMTs are skilled at helping children and adults gain control over muscular habits, including those involved in a tongue thrust. For those who struggle with tongue thrusting, eliminating the habit is not always easy. This is due to the fact that the muscles of the face and mouth have been programmed over many years to adopt this behavior. It takes neuromuscular re-education to correct this habit and relearn the correct</p>
+                        <h4>Orofacial myofunctional therapy in tongue thrust helps to:</h4>
+                        <ul>
+                            <li>Improve tongue elevation strength</li>
+                            <li>Correct tongue resting posture</li>
+                            <li>Correct tongue placement while swallowing</li>
+                            <li>Improve tongue motility</li>
+                            <li>Improve maxillary constriction</li>
+                            <li>Prevent relapse of corrected malocclusion</li>
+                            <li>Improve sleep-disordered breathing</li>
+                        </ul>
                     </div>
                     <div>
                         <div className={"tongue-thrust-image-container"}>
@@ -42,24 +52,14 @@ export default function TongueThrust(): ReactElement {
                         <div className={"tongue-thrust-image-container"}>
                             <img id={'tongue-thrust-image'} src={TongueThrustThree} alt={"Orthodontic damage due to tongue thrust example 3"} loading={'lazy'}/>
                         </div>
-                        <div className={"tongue-thrust-image-container"}>
-                            <img id={'tongue-thrust-image'} src={TongueThrustFour} alt={"Orthodontic damage due to tongue thrust example 4"} loading={'lazy'}/>
-                        </div>
-                        <div className={"tongue-thrust-image-container"}>
-                            <img id={'tongue-thrust-image'} src={TongueThrustSix} alt={"Orthodontic damage due to tongue thrust example 6"} loading={'lazy'}/>
-                        </div>
+                        {/*<div className={"tongue-thrust-image-container"}>*/}
+                        {/*    <img id={'tongue-thrust-image'} src={TongueThrustFour} alt={"Orthodontic damage due to tongue thrust example 4"} loading={'lazy'}/>*/}
+                        {/*</div>*/}
+                        {/*<div className={"tongue-thrust-image-container"}>*/}
+                        {/*    <img id={'tongue-thrust-image'} src={TongueThrustSix} alt={"Orthodontic damage due to tongue thrust example 6"} loading={'lazy'}/>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
-                <h4>Orofacial myofunctional therapy in tongue thrust helps to:</h4>
-                <ul>
-                    <li>Improve tongue elevation strength</li>
-                    <li>Correct tongue resting posture</li>
-                    <li>Correct tongue placement while swallowing</li>
-                    <li>Improve tongue motility</li>
-                    <li>Improve maxillary constriction</li>
-                    <li>Prevent relapse of corrected malocclusion</li>
-                    <li>Improve sleep-disordered breathing</li>
-                </ul>
                 <div className={'tongue-thrust-videos-wrapper'}>
                     <div className={'tongue-thrust-video-container'}>
                         <h4>This video represents correct swallowing:</h4>
