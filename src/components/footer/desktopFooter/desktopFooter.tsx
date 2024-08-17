6b64d732449b931bd2bdaafb7e844c5235342224
Brandon Cantello <bcantello@gmail.com>
2024-08-16 19:46:42 -0700

    Updates footer for mobile and desktop

--- src/components/footer/desktopFooter/desktopFooter.tsx
+++ src/components/footer/desktopFooter/desktopFooter.tsx
@@ -2,7 +2,6 @@ import {ReactElement} from "react";
 import SimpleButton from "../../simpleButton/simpleButton";
 import Facebook from '../../../assets/icons/facebook.svg';
 import Instagram from '../../../assets/icons/instagram.svg';
-// import LinkedIn from '../../../assets/icons/linkedin.svg';
 import Address from '../../../assets/icons/address.png';
 import Phone from '../../../assets/icons/phone.png';
 import Email from '../../../assets/icons/email.png';
@@ -20,42 +19,42 @@ export default function DesktopFooter(): ReactElement {
                     <div className={'footer-contact-container'}>
                         <div className={'footer-phone-container'}>
                             <a
-                                href={"tel:805-453-1343"}
+                                href={"tel:719-648-5582"}
                                 target={"_blank"}
                                 rel={"noreferrer"}
                             >
                                 <img src={Phone} alt={'Address drop pin'}/>
                             </a>
                             <a
-                                href={"tel:805-453-1343"}
+                                href={"tel:719-648-5582"}
                                 target={"_blank"}
                                 rel={"noreferrer"}
                             >
-                                805-453-1343
+                                719-648-5582
                             </a>
                         </div>
                         <div className={'footer-email-container'}>
-                            <a href="mailto:nicole@santabarbaramyo.com">
+                            <a href="mailto:contact@getrevomag.com">
                                 <img src={Email} alt={'Address drop pin'}/>
                             </a>
-                            <a href="mailto:nicole@santabarbaramyo.com">
-                                nicole@santabarbaramyo.com
+                            <a href="mailto:contact@getrevomag.com">
+                                contact@getrevomag.com
                             </a>
                         </div>
                         <div className={'footer-address-container'}>
                             <a
-                                href={"https://www.google.com/maps/search/?api=1&query=5290%20Overpass%20Road%20Suite%20211%20Santa%20Barbara%2C%20California%2C%2093111"}
+                                href={""}
                                 target={"_blank"}
                                 rel={"noreferrer"}
                             >
                                 <img src={Address} alt={'Address drop pin'}/>
                             </a>
                             <a
-                                href={"https://www.google.com/maps/search/?api=1&query=5290%20Overpass%20Road%20Suite%20211%20Santa%20Barbara%2C%20California%2C%2093111"}
+                                href={""}
                                 target={"_blank"}
                                 rel={"noreferrer"}
                             >
-                                5290 Overpass Road Suite 211, Santa Barbara CA 93111
+                                Someplace in Massachusetts
                             </a>
                         </div>
                     </div>
@@ -78,23 +77,14 @@ export default function DesktopFooter(): ReactElement {
                                 <img src={Instagram} alt={'instagram link'}/>
                             </a>
                         </div>
-                        {/*<div className={'social-icon'}>*/}
-                        {/*    <a*/}
-                        {/*        href={"https://www.linkedin.com/company/santa-barbara-myo/"}*/}
-                        {/*        target={"_blank"}*/}
-                        {/*        rel={"noreferrer"}*/}
-                        {/*    >*/}
-                        {/*        <img src={LinkedIn} alt={'linkedin link'}/>*/}
-                        {/*    </a>*/}
-                        {/*</div>*/}
                     </div>
                 </div>
                 <div className={'footer-divider'}/>
                 <div className={'footer-content-right'}>
-                    <h2>Ready To Learn More?</h2>
+                    <h2>Ready for a better Revolver Reload?</h2>
                     {/*Book an Appointment through Simple Practice*/}
                     <SimpleButton
-                        buttonText={'Request a Consultation'}
+                        buttonText={'Buy RevoMag'}
                         destination={'https://hushforms.com/santabarbaramyo_consultation'}
                     />
                 </div>
@@ -107,9 +97,6 @@ export default function DesktopFooter(): ReactElement {
                                 <a href={'/home'}>Home</a>
                             </li>
                             <li>
-                                <a href={'/how-it-works'}>How It Works</a>
-                            </li>
-                            <li>
                                 <a href={'/about'}>About</a>
                             </li>
                         </ul>
@@ -117,27 +104,6 @@ export default function DesktopFooter(): ReactElement {
                     <div className={'footer-sitemap-information'}>
                         <h4>Information</h4>
                         <ul>
-                            <li>
-                                <a href={'/mouth-breathing'}>Mouth Breathing</a>
-                            </li>
-                            <li>
-                                <a href={'/snoring-and-sleep-apnea'}>Snoring/Sleep Apnea</a>
-                            </li>
-                            <li>
-                                <a href={'/adhd-and-sleep-disordered-breathing'}>ADHD</a>
-                            </li>
-                            <li>
-                                <a href={'/tmj-pain'}>TMJ Pain</a>
-                            </li>
-                            <li>
-                                <a href={'/tongue-tie'}>Tongue Tie</a>
-                            </li>
-                            <li>
-                                <a href={'/thumb-and-finger-sucking'}>Thumb Sucking</a>
-                            </li>
-                            <li>
-                                <a href={'/tongue-thrust'}>Tongue Thrust</a>
-                            </li>
                             <li>
                                 <a href={'/resources'}>Resources</a>
                             </li>
@@ -147,12 +113,6 @@ export default function DesktopFooter(): ReactElement {
                         <h4>Connect</h4>
                         <ul>
                             <li>
-                                <a href={'/book-appointment'}>Schedule Appointment</a>
-                            </li>
-                            <li>
-                                <a href={'/refer'}>Refer a Patient</a>
-                            </li>
-                            <li>
                                 <a href={'/contact'}>Contact</a>
                             </li>
                         </ul>
@@ -161,7 +121,7 @@ export default function DesktopFooter(): ReactElement {
             </div>
             <div className={'footer-right-links'}>
                 <div>
-                    <span>&copy; Santa Barbara Myo 2023  |  </span>
+                    <span>&copy; Goodlee CO 2024  |  </span>
                     <a href={'/disclaimer'}>Disclaimer</a>
                     <span>  |  </span>
                     <a href={'/privacy'}>Privacy</a>
