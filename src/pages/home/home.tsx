8b764bf4125b6e767b33e4719647c250f33d4068
Brandon <bcantello@gmail.com>
2025-01-05 15:47:57 -0800

    Adds video modal component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,6 +4,7 @@ import HeroVideo from "../../assets/bannerImages/HeroVideo.mp4";
 import './home.scss';
 import SimpleButton from "../../components/simpleButton/simpleButton";
 import Hero from "../../assets/bannerImages/RevomagWEBBanner.jpg";
+import VideoModal from "../../components/VideoModal/VideoModal";
 
 /**
  * Renders home page
@@ -24,9 +25,14 @@ export default function Home(): ReactElement {
                 </video>
             </div>
             <section className={'home-content'}>
-                <div className={'revomag-info'}>
-                    <SimpleButton buttonText={"Promotional Video"} destination={'/'}/>
-                </div>
+                {/*<div className={'revomag-info'}>*/}
+                {/*    <SimpleButton buttonText={"Promotional Video"} destination={'/'}/>*/}
+                {/*</div>*/}
+                <VideoModal
+                    videoId={"VHS8xIsR9XY"}
+                    buttonText={'View Promotional Video'}
+                    buttonClassName={'revomag-promo-button'}
+                />
             </section>
         </>
     );
