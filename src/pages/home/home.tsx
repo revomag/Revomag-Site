90592dd53c3595b93cfe59906e306fc9327dbac2
Brandon <bcantello@gmail.com>
2025-01-06 18:09:29 -0800

    Fixes modal bug

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,6 @@
 import {ReactElement} from "react";
 import Navigation from "../../components/navigation/navigation";
-import HeroVideo from "../../assets/bannerImages/HeroVideo.mp4";
+// import HeroVideo from "../../assets/bannerImages/HeroVideo.mp4";
 import './home.scss';
 import Hero from "../../assets/bannerImages/RevomagWEBBanner.jpg";
 import VideoModal from "../../components/VideoModal/VideoModal";
@@ -17,14 +17,14 @@ export default function Home(): ReactElement {
                 <div className={'hero-image-container'}>
                     <img src={Hero} alt={'revomag'} title={'revomag'} />
                 </div>
-            </div>
-            <div className={'video-modal-button-wrapper'}>
-                <VideoModal
-                    videoId={"VHS8xIsR9XY"}
-                    text={'View Promotional Video'}
-                    baseClassName={'revomag-promo-button'}
-                    displayType={'button'}
-                />
+                <div className={'video-modal-button-wrapper'}>
+                    <VideoModal
+                        videoId={"VHS8xIsR9XY"}
+                        text={'View Promotional Video'}
+                        baseClassName={'revomag-promo-button'}
+                        displayType={'button'}
+                    />
+                </div>
             </div>
             {/*<section className={'home-content'}>*/}
             {/*    <VideoModal*/}
